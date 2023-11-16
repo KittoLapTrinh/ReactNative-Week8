4b88245e503f8e41da60111fc37da752cc378694
Kitto_LapTrinh <96908923+KittoLapTrinh@users.noreply.github.com>
2023-11-16 13:50:16 +0700

    Add files via upload

--- components/Screen1.js
+++ components/Screen1.js
@@ -1,22 +1,40 @@
 import {View,Button,Image,TextInput, Text, StyleSheet, TouchableOpacity} from 'react-native'
-import { useEffect, useState } from 'react'
+import React,{ useEffect, useState } from 'react'
 
 
 
 function Screen1({navigation,}){
-    const [data, setData] = useState([]);
-    const [emailn, setEmail] = useState('')
+    const [id, setId] = useState('')
+    const [idErr, setIdErr] = useState('')
+
+    const nextScreen2 = async ()=>{
+        try{
+            const response = await fetch(`https://6554786b63cafc694fe68232.mockapi.io/user/v1/${id}`)
+            if(response.ok){
+                const userData = await response.json();
+                console.log('Suscessfully!')
+                navigation.navigate('Screen2', {userData})
+            }else{
+                console.error('Error! No User with Id here', setIdErr)
+     
+            }
+        }catch(error){
+            console.error('Error! Data null', error);
+        }
+    }
+    // const [data, setData] = useState([]);
+    // const [emailn, setEmail] = useState('')
 
 
-    fetch("https://653f68399e8bd3be29e07f8f.mockapi.io/api/v1/user")
-        .then(response=>{
-        if (response.ok)
-            return response.json()
-        })
-        .then(dataO=>{
-        if(data.length==0)
-            setData(dataO)
-    })
+    // fetch("https://653f68399e8bd3be29e07f8f.mockapi.io/api/v1/user")
+    //     .then(response=>{
+    //     if (response.ok)
+    //         return response.json()
+    //     })
+    //     .then(dataO=>{
+    //     if(data.length==0)
+    //         setData(dataO)
+    // })
 
     return(
         <View style = {styles.container}>
@@ -30,10 +48,17 @@ function Screen1({navigation,}){
 
             <View style={{alignItems: 'center', justifyContent: 'center', flexDirection: 'row', paddingVertical: 20}}>
                 <Image style={{width: 20, height: 20, borderColor: 'black',}} source={require('../assets/enter.png')}></Image>
-                <TextInput  placeholder='Enter your name' style={{borderWidth: 1, color: '#C4C4C4', borderRadius: 10, height: 50, width: 300}} onChangeText={setEmail}></TextInput>
+                <TextInput  placeholder='Enter your name' style={{borderWidth: 1, color: '#C4C4C4', borderRadius: 10, height: 50, width: 300}} value={id} onChangeText={text => setId(text)} ></TextInput>
             </View>
+            <Text>{idErr}</Text>
+            <TouchableOpacity style={{ justifyContent: 'center', alignItems: 'center', backgroundColor: '#00bdd6', borderRadius: 30, marginHorizontal: 30, height: 50 }}
+                    onPress={nextScreen2}
+                        
+                    >
+                    <Text style={{color: 'white' }}>GET STARTED</Text>
+                </TouchableOpacity>
             
-            {data.length != 0 &&
+            {/* {data.length != 0 &&
                 <TouchableOpacity style={{ justifyContent: 'center', alignItems: 'center', backgroundColor: '#00bdd6', borderRadius: 30, marginHorizontal: 30, height: 50 }}
                     onPress={
                         () => {
@@ -48,7 +73,7 @@ function Screen1({navigation,}){
                     >
                     <Text style={{color: 'white' }}>GET STARTED</Text>
                 </TouchableOpacity>
-            }
+            } */}
 
            
         </View>
--- components/Screen2.js
+++ components/Screen2.js
@@ -1,38 +1,48 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRoute } from 'react'
 import {View,Button,Image,TextInput, Text, StyleSheet, TouchableOpacity, FlatList} from 'react-native'
 import { ScrollView } from 'react-native-web'
+import { useNavigation } from '@react-navigation/native';
 
-
-
+import Tick from '../assets/tick.png'
+import Note from '../assets/note.png'
 
 
 function Screen2({navigation, route}){
 
-    // const {name} = route.params
-    const [todos, setTodos] = useState({})
+    const [userData, setUserData] = useState({});
+    const [tasks, setTasks] = useState([]);
 
-
-    var [data, setData] = useState([]);
-
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/api/v1/user', {
-        method: 'GET',
-        headers: {'content-type':'application/json'},
-        }).then(res => {
-        if (res.ok) {
-            return res.json();
+    useEffect(()=>{
+        if(route.params && route.params.userData){
+            setUserData(route.params.userData);
+            fetchUserTasks(route.params.userData.id);
         }
-        }).then(todolist => {
-            setTodos(todolist)
-        }).catch(error => {
-            console.log("Error ", error);
-        })
-    }, [])
+    }, [route.params])
 
+    const addTask = async (task) =>{
+        try{
+            await fetch(`https://6554786b63cafc694fe68232.mockapi.io/user/v1/${userData.id}/tasks`,{
+                method: 'POST',
+                headers:{
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({description: task}),
+            });
+            fetchUserTasks();
+        }catch(error){
+            console.error('Error add task!', error)
+        }
+    }
 
-    
-   
-
+    const fetchUserTasks = async ()=>{
+        try{
+            const response = await fetch(`https://6554786b63cafc694fe68232.mockapi.io/user/v1/${userData.id}/tasks`)
+            const tasksData = await response.json();
+            setTasks(tasksData);
+        }catch(error){
+            console.error('Error fetching user tasks', error)
+        }
+    }
     return(
         <View style = {styles.container}>
             <View style={{flexDirection: 'row',  alignItems: 'center', paddingBottom: 30, paddingHorizontal: 20 , }}>
@@ -41,13 +51,13 @@ function Screen2({navigation, route}){
                 </TouchableOpacity>
           
                 <TouchableOpacity  style={{paddingLeft: 130}} onPress={()=>{navigation.navigate('Screen3')}}>
-                    <Image style={{width: 40, height: 40, }} source={require('../assets/user.png')}></Image>
+                    <Image style={{width: 40, height: 40, }} source={{uri: userData.avt}}></Image>
                 </TouchableOpacity>
                     
                 
                 <View style={{justifyContent: 'center'}}>
-                    <Text style={{fontSize: 20, fontWeight: 'bold'}}>Hi Twinkle</Text>
-                    <Text style={{fontSize: 15}}>Have agrate day a head</Text>
+                    <Text style={{fontSize: 20, fontWeight: 'bold'}}>{userData.name}</Text>
+                    <Text style={{fontSize: 15}}>{userData.description}</Text>
                 </View>
               
             
@@ -56,25 +66,20 @@ function Screen2({navigation, route}){
                 <Image style={{width: 40, height: 40, }} source={require('../assets/search.png')}></Image>
                 <TextInput placeholder='Search' style={{color: '#C4C4C4', width: 300}}></TextInput>
             </View>
-            
-            
-            {data.map((item) => (
-                    <View style={{ marginTop: 60 }}>
-                        <TouchableOpacity>
-                            <Image
-                                source={{ uri: item.image }}
-                                style={{ width: 200, height: 62, borderRadius: 6, resizeMode: 'cover' }}
-                            />
-                        </TouchableOpacity>
-                        <TouchableOpacity onPress={() => updateCompletedTodo(item.id)}>
-                            <Icon name="check-square" size={24} color="green" />
-                        </TouchableOpacity>
-                        <Text style={{fontSize: '20px', fontWeight: 'bold', marginLeft: '10px', marginRight: 'auto'}}>{item.description}</Text>
-                    </View>
-                ))}
+            <View style={{ marginTop: 60 }}>
+                <FlatList data={userData.tasks} renderItem={({item})=>(
 
-            
-  
+                    <View style={{flexDirection: 'row', borderWidth: 1, borderRadius: 10, paddingVertical: 10}}>
+                        <Image source={require('../assets/tick.png')} style={{width: 20, height: 20, marginLeft: 20}}/>                     
+                        <Text style={{fontSize: '20px', fontWeight: 'bold', marginLeft: '10px', marginRight: 'auto'}}>{item}</Text>
+                        <Image source={require('../assets/note.png')} style={{width: 20, height: 20, marginRight: 20}}/>
+                    </View>
+                )}>
+                    
+                    
+                </FlatList>
+
+            </View>
             <View style={{borderRadius: 30, marginHorizontal: 170, paddingTop: 50}}>
                 <Button title='+' color={'#26c3d9'} style={{}} onPress={()=>{navigation.navigate('Screen3')}}></Button>
             </View>
